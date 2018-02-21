c299c96eaede423d1fcebfdda4b6e630c8f7d696
Daniel Myung <wowplus2@gmail.com>
2018-02-21 17:38:48 +0900

    Add files via upload

--- src/main/com/nettybook/ch3/EchoServerWithOption.java
+++ src/main/com/nettybook/ch3/EchoServerWithOption.java
@@ -3,6 +3,7 @@ package com.nettybook.ch3;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
@@ -11,23 +12,24 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 
-public class EchoServerV3 
+public class EchoServerWithOption 
 {
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		EventLoopGroup bossGrp = new NioEventLoopGroup();
+		EventLoopGroup bossGrp = new NioEventLoopGroup(1);
 		EventLoopGroup workerGrp = new NioEventLoopGroup();
 		
 		try {
 			ServerBootstrap sb = new ServerBootstrap();
+			
 			sb.group(bossGrp, workerGrp)
 			.channel(NioServerSocketChannel.class)
+			.option(ChannelOption.SO_BACKLOG, 1)
 			.childHandler(new ChannelInitializer<SocketChannel>() {
 				@Override
-				protected void initChannel(SocketChannel ch) {
+				protected void initChannel(SocketChannel ch) throws Exception {
 					// TODO Auto-generated method stub
 					ChannelPipeline p = ch.pipeline();
-					//p.addLast(new LoggingHandler(LogLevel.DEBUG));
 					p.addLast(new LoggingHandler(LogLevel.INFO));
 					p.addLast(new EchoServerHandler());
 				}
