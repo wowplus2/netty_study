05c0b9ad05b45e32ad01fcaba65747ba48ec6200
Daniel Myung <wowplus2@gmail.com>
2018-02-26 19:48:45 +0900

    Add files via upload

--- src/main/com/nettybook/ch5/EchoServerHandler.java
+++ src/main/com/nettybook/ch5/EchoServerHandler.java
@@ -10,7 +10,11 @@ public class EchoServerHandler extends ChannelInboundHandlerAdapter
 	@Override
 	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
 		// TODO Auto-generated method stub
+		/* 수신된 데이터를 클라이언트 소켓 버퍼에 기록하고 
+		 * 버퍼의 데이터를 채널로 전송하는 비동기 메서드인 writeAndFlush를 호출하고 ChannelFuture 객체를 돌려받는다. */
 		ChannelFuture chFuture = ctx.writeAndFlush(msg);
+		/* ChannelFuture 객체에 채널을 종료하는 리스너를 등록한다.
+		 * ChannelFutureListener.CLOSE 리스너는 네티가 제공하는 기본 리스너로서 ChannelFuture 객체가 완료 이벤트를 수신할 때 수행된다. */
 		chFuture.addListener(ChannelFutureListener.CLOSE);
 	}
 
